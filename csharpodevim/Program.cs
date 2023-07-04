4cc2928df25f251542c4d0bb4499829cee5317f4
Ali Kemal Gündüz <kemall.gunduz@icloud.com>
2023-07-04 15:55:47 +0300

    csharpodev

---
+++ csharpodevim/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpodevim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world !");
+            byte b = 5;
+            sbyte c = 5;
+            short s = 5;
+            ushort us = 5;
+
+            Int16 i16 = 2;
+            int i = 2;
+            Int32 i32 = 2;
+            Int64 i64 = 2;
+        
+        }
+    }
+}
